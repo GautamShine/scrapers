0f841a0e9aebdf7d43d88e69b9416d5117592409
Gautam Shine <gshine@stanford.edu>
2016-05-10 15:09:28 -0700

    Generalized source parser

--- scraper.py
+++ scraper.py
@@ -6,23 +6,33 @@ import re
 import numpy as np
 
 class Scraper:
-    def __init__(self, base_url):
-        self.base_url = base_url
+    """
+    Base class for scraping sites with Beautiful Soup and regexes
+    """
+    def __init__(self):
+        pass
 
-    def get_urls(self):
+    def get_urls(self, base_url):
         urls = None
         return urls
 
-    def parse_src(self, url, tag, regex):
+    def parse_url(self, url, tag, attrs=None, target=None, regex=None):
         """
         Applies a regex to a tag in a url's source
         """
         response = requests.get(url)
         soup = BeautifulSoup(response.content, 'lxml')
-        parse = soup.find_all(class_=tag)
-        target = re.findall(regex, str(parse))
+        parse = soup.find(tag, attrs)
 
-        return target
+        # Optionally extract a target attribute
+        if target:
+            parse = parse[target]
+
+        # Optionally apply a regex
+        if regex:
+            parse = re.findall(regex, str(parse))
+
+        return parse
 
     def store(self, csv, **kwargs):
         pass
